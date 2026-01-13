453ed2b6fff16b41fef855a77f322f8a23628f12
Irenecia <irenecia77@gmail.com>
2026-01-13 17:28:54 +0000

    añadido footer con instagram y perfil de githut

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,16 @@
 export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center">
 		
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="https://www.instagram.com/irenecia/?hl=es">Irene Cia</a>
-		</p>
+	
+
+		<p> Made with <i className="fa fa-heart text-danger" /> by Irene Cia </p> 
+	<a href="https://www.instagram.com/irenecia/?hl=es" target="_blank" rel="noopener noreferrer" style={{textDecoration: `none`}}>
+   <i class="fa-brands fa-instagram" style= {{color: '#e0dbdd', fontSize: `24px`, marginLeft: `10px`}}></i> </a>
+
+   <a href= "https://github.com/Irenecia" target="_blank" rel="noopener noreferrer" style={{textDecoration: `none`}}>
+   <i className="fa-brands fa-github" style= {{color:'#e0dbdd', fontSize: `24px`, marginLeft: `10px`}} ></i>
+   </a>
+	
+	
 	</footer>
 );
